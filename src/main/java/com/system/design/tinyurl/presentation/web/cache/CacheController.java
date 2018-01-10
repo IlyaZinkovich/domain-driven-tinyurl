8a974102f7b68c7fd1eaf6f8f96e3a79aaad067a
Ilya Zinkovich <ilya.zinkovich@gmail.com>
2018-01-10 14:02:07 +0300

    Added redirects to original URL

--- src/main/java/com/system/design/tinyurl/presentation/web/cache/CacheController.java
+++ src/main/java/com/system/design/tinyurl/presentation/web/cache/CacheController.java
@@ -4,13 +4,10 @@ import com.google.gson.Gson;
 import com.system.design.tinyurl.application.cache.CacheService;
 import com.system.design.tinyurl.domain.cache.query.OriginalUrlByHashQuery;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import static org.springframework.http.HttpStatus.NOT_FOUND;
-import static org.springframework.http.HttpStatus.OK;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class CacheController {
@@ -25,9 +22,9 @@ public class CacheController {
     }
 
     @GetMapping(path = "/cache/{urlHash}")
-    public ResponseEntity<String> getOriginalUrlByHash(@PathVariable String urlHash) {
+    public RedirectView getOriginalUrlByHash(@PathVariable String urlHash) {
         return cacheService.getOriginalUrlByHash(new OriginalUrlByHashQuery(urlHash))
-                .map(originalUrl -> new ResponseEntity<>(gson.toJson(new OriginalUrlResponse(originalUrl)), OK))
-                .orElse(new ResponseEntity<>("", NOT_FOUND));
+                .map(RedirectView::new)
+                .orElse(new RedirectView("/404.html", true));
     }
 }
